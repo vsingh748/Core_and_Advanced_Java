a52363d8b3665821ae9b5b668bbeb2ea9be28956
Vishruti Singh <vsingh748@gmail.com>
2017-09-11 10:39:37 +0530

    The first program
    For basic java demonstration purpose

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+//as this is a ritual before starting any kind of programming.
+//this is a basic java code
+
+class Hello //class name must start with a capital letter
+{
+  public static void main(String [] args)
+  {
+    System.out.println("Hello World");
+    }
+  }
+}
